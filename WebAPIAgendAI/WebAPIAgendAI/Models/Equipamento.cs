de496061f66fc262f49cc4bf083dcc4f3ce068ae
thaisoliveira1356480 <thais.oliveira.1356480@sga.pucminas.br>
2023-04-21 22:15:19 -0300

    Upadate BD e Models

---
+++ WebAPIAgendAI/WebAPIAgendAI/Models/Equipamento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAgendAI.Models
+{
+    public class Equipamento
+    {
+        [Key]
+        [Required(ErrorMessage = "Obrigatório Informar o modelo!")]
+        public int NumeroSerie { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o modelo!")]
+        public string Modelo { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o status")]
+        public StatusEquipamento Status { get; set; }
+    }
+
+    public enum StatusEquipamento
+    {
+        Disponivel,
+        Indisponivel
+    }
+}
