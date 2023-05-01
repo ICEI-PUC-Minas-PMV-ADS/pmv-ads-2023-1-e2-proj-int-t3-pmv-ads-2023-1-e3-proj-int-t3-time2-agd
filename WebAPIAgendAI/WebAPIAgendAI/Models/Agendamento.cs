2555855e98ade2eb3acc697152646bd36eba7a42
thaisoliveira1356480 <thais.oliveira.1356480@sga.pucminas.br>
2023-05-01 20:14:25 -0300

    nova solução agendai

---
+++ WebAPIAgendAI/WebAPIAgendAI/Models/Agendamento.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIAgendAI.Models
+{
+    public class Agendamento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public int Quantidade { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string Turma { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        [EmailAddress(ErrorMessage = "Entre com um email válido")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailInstitucional { get; set; }
+        public int FuncionarioId { get; set; }
+
+        [ForeignKey("FuncionarioId")]
+        public Funcionario Funcionario { get; set; }
+
+    }
+}
