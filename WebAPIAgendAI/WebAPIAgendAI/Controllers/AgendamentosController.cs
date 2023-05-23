f7211dfb2ef8c82d75ac7346e83f2fd6e0477121
thaisoliveira1356480 <thais.oliveira.1356480@sga.pucminas.br>
2023-05-22 22:07:26 -0300

    update solução

---
+++ WebAPIAgendAI/WebAPIAgendAI/Controllers/AgendamentosController.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIAgendAI.Models;
+
+namespace WebAPIAgendAI.Controllers
+{
+    [Authorize]
+    public class AgendamentosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public AgendamentosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Agendamentos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Agendamentos.ToListAsync());
+        }
+
+        // GET: Agendamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agendamento = await _context.Agendamentos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (agendamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(agendamento);
+        }
+
+        // GET: Agendamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Agendamentos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Data,Quantidade,Turma,EmailInstitucional,FuncionarioId")] Agendamento agendamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(agendamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(agendamento);
+        }
+
+        // GET: Agendamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agendamento = await _context.Agendamentos.FindAsync(id);
+            if (agendamento == null)
+            {
+                return NotFound();
+            }
+            return View(agendamento);
+        }
+
+        // POST: Agendamentos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Data,Quantidade,Turma,EmailInstitucional,FuncionarioId")] Agendamento agendamento)
+        {
+            if (id != agendamento.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(agendamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AgendamentoExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(agendamento);
+        }
+
+        [Authorize(Roles = "Administrador")]
+        // GET: Agendamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var agendamento = await _context.Agendamentos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (agendamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(agendamento);
+        }
+        [Authorize(Roles = "Administrador")]
+        // POST: Agendamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var agendamento = await _context.Agendamentos.FindAsync(id);
+            _context.Agendamentos.Remove(agendamento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AgendamentoExists(int id)
+        {
+            return _context.Agendamentos.Any(e => e.Id == id);
+        }
+
+    }
+}
